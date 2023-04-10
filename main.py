b230224e4b01a21971fa8c1ff9aa03e869ea32ce
Михаил <guicokiuytr@gmail.com>
2023-04-10 21:50:26 +0600

    register and login (не делал изменений в верстке)

--- main.py
+++ main.py
@@ -3,18 +3,19 @@ import datetime
 from flask import Flask, render_template, redirect, request, make_response, abort, jsonify
 
 from data import db_session
+from data.users import User
 
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 
-from forms.user import RegisterForm
+from forms.user import RegisterForm, LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-# app.config['PERMANENT_SESSION_LIFETIME'] = datetime.timedelta(
-#     days=365
-# )
-# login_manager = LoginManager()
-# login_manager.init_app(app)
+app.config['PERMANENT_SESSION_LIFETIME'] = datetime.timedelta(
+    days=365
+)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
 @app.route("/")
@@ -25,16 +26,59 @@ def index():
 @app.route('/register', methods=['GET', 'POST'])
 def reqister():
     form = RegisterForm()
-    # if form.validate_on_submit():
-    #     if form.password.data != form.password_again.data:
-    #         return render_template('register.html', title='Регистрация',
-    #                                form=form,
-    #                                message="Пароли не совпадают")
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            surname=form.surname.data,
+            username=form.username.data,
+            email=form.email.data,
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
     return render_template('register.html', title='Регистрация', form=form)
 
 
-def main():
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
 
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+def main():
+    db_session.global_init("databases/technomart.db")
     app.run()
 
 
