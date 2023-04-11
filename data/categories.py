c7d5e6055df720f9bf30cf639a58ee6df9ca3c35
MLGRussianXP <daniilkoles74@gmail.com>
2023-04-11 17:47:11 +0600

    add products field to category orm class

--- data/categories.py
+++ data/categories.py
@@ -1,5 +1,6 @@
 import sqlalchemy
 from flask_login import UserMixin
+from sqlalchemy import orm
 from sqlalchemy_serializer import SerializerMixin
 
 from .db_session import SqlAlchemyBase
@@ -11,3 +12,4 @@ class Category(SqlAlchemyBase, UserMixin, SerializerMixin):
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    products = orm.relationship("Product", back_populates='category')
