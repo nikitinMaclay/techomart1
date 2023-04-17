938c83955e7c50a5ea76a09ab1bcfd3b3808a1da
Михаил <guicokiuytr@gmail.com>
2023-04-17 20:32:44 +0600

    все по кайфу

---
+++ data/products.py
@@ -0,0 +1,19 @@
+import sqlalchemy
+from flask_login import UserMixin
+from sqlalchemy import orm
+from sqlalchemy_serializer import SerializerMixin
+
+from .db_session import SqlAlchemyBase
+
+
+class Product(SqlAlchemyBase, UserMixin, SerializerMixin):
+    __tablename__ = 'products'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    image = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    price = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    discount_price = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    category_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("categories.id"))
+    category = orm.relationship('Category')
